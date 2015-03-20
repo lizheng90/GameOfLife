fa8044c3507bf05c16e8a23b3665e22c7c6631b6
lizheng90 <jackyliz900215@gmail.com>
2015-03-20 14:26:42 -0700

    14836 Mobile Game

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,21 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Zheng Li on 1/29/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+
+-(void) evolveStep;
+-(void) countNeighbors;
+-(void) updateCreatures;
+-(BOOL) isIndexValidForX:(int)x andY:(int)y;
+
+@end
